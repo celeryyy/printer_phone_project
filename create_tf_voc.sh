3a1803454dfa6f892a09797a3caa43d0ea60c096
celeryyy <74958971+celeryyy@users.noreply.github.com>
2022-06-30 20:55:28 +0800

    Update create_tf_voc.sh

--- create_tf_voc.sh
+++ create_tf_voc.sh
@@ -3,7 +3,7 @@ LABELMAP=/content/printer_phone_project/data/label_map.pbtxt
 OUTPUT_DIR=/content/printer_phone_project/data
 TEST_RATIO=0.2
 
-python create_tf_records_voc.py \
+!python create_tf_records_voc.py \
       --data_dir="${DATA_DIR}" \
       --label_map="${LABELMAP}" \
       --test_ratio="${TEST_RATIO}" \
