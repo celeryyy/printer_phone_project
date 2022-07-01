22dda2e1f158ed6006f3a7c42e664ed90bcf10db
celeryyy <74958971+celeryyy@users.noreply.github.com>
2022-07-01 11:03:30 +0800

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,4 +1,4 @@
-MODEL=centernet_resnet50_v1_fpn_512x512_coco17_tpu-8
+MODEL=centernet_resnet50_v2_512x512_coco17_tpu-8
 EXPERIMENT=run1
 PIPELINE_CONFIG_PATH=/home/ubuntu/printer_phone_project/models/${MODEL}/${EXPERIMENT}/pipeline.config
 MODEL_DIR=/home/ubuntu/printer_phone_project/models/${MODEL}/${EXPERIMENT}/
